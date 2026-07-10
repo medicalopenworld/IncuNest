10542d74a3bf7db26a43548a87c0869103bf6fd7
pablo18393 <pablosanchez18393@hotmail.com>
2026-07-11 01:30:20 +0200

    refactor(motherboard): collapse board.h pin/config chain to HW_NUM>=16 (phase 2a of legacy cleanup)

--- Firmware/motherBoard/include/config/board.h
+++ Firmware/motherBoard/include/config/board.h
@@ -41,10 +41,6 @@
 #define DISPLAY_SPI_CLK SPI_CLOCK_DIV16
 #endif
 
-#if (HW_NUM <= 8)
-#define ANALOG_TO_AMP_FACTOR 0.2
-#define CURRENT_MEASURES_AMOUNT 20
-#endif
 #if (HW_NUM <= 6)
 #define HUMIDIFIER_INTERFACE HUMIDIFIER_BINARY
 #elif (HW_NUM <= 8)
@@ -56,7 +52,6 @@
 #endif
 
 #define GPIO_EXP_BASE 100 // To differentiate with ESP32 GPIO
-#if (HW_NUM >= 16)
 // Power / control
 #define PWR_EN 2
 #define ON_OFF_SWITCH 4
@@ -116,201 +111,6 @@
 #define ENC_B FAKE_PIN
 #undef TFT_CS
 #define TFT_CS FAKE_PIN
-#elif (HW_NUM == 15)
-// PINOUT
-#define BUZZER 5 // Set FAKE_PIN to mute buzzer
-#define LED 7
-#define I2C_SDA 8
-#define I2C_SCL 9
-#define BABY_NTC_PIN 10
-#define FAN 12
-#define PHOTOTHERAPY 13
-#define ACTUATORS_EN 14
-#define HEATER 16
-#define AFE44XX_CS 21
-#define FAN_SPEED_FEEDBACK 38
-#define AFE_ADC_READY 45
-#define GSM_UART_TX_PIN 48
-#define GSM_UART_RX_PIN 47
-// AFE SPI bus (shared with TFT SPI, pins from build flags TFT_SCLK/MISO/MOSI)
-#define AFE_SCK 36
-#define AFE_MISO 37
-#define AFE_MOSI 35
-
-#define FAKE_PIN 46
-
-#define SCREENBACKLIGHT FAKE_PIN
-#define ON_OFF_SWITCH FAKE_PIN
-#define AFE44XX_PWDN_PIN FAKE_PIN
-#define GPRS_PWRKEY FAKE_PIN
-#undef TFT_DC
-#define TFT_DC FAKE_PIN
-#define ENC_SWITCH FAKE_PIN
-#define ENC_A FAKE_PIN
-#define ENC_B FAKE_PIN
-#undef TFT_CS
-#define TFT_CS FAKE_PIN
-
-#elif (HW_NUM == 14)
-// PINOUT
-#define TFT_DC 0
-#define AFE44XX_CS 2
-#define ENC_SWITCH 4
-#define BUZZER 5
-#define FAN 12
-#define PHOTOTHERAPY 13
-#define ACTUATORS_EN 14
-#define TFT_CS 15
-#define I2C_SDA 21
-#define I2C_SCL 22
-#define ENC_A 25
-#define HEATER 27
-#define SCREENBACKLIGHT 33
-#define ENC_B 34
-#define FAN_SPEED_FEEDBACK 35
-#define BABY_NTC_PIN 39
-#define GSM_UART_TX_PIN 16
-#define GSM_UART_RX_PIN 17
-#define AFE4490_ADC_READY NULL
-#define ON_OFF_SWITCH NULL
-#define AFE44XX_PWDN_PIN NULL
-#define GPRS_PWRKEY NULL
-
-#elif (HW_NUM >= 13)
-// PINOUT
-// #define TFT_DC 0
-#define AFE44XX_CS 2
-#define ENC_SWITCH 4
-#define BUZZER 5
-#define FAN 12
-#define PHOTOTHERAPY 13
-#define ACTUATORS_EN 14
-#define GSM_UART_TX_PIN 16
-#define GSM_UART_RX_PIN 17
-// #define TFT_CS 15
-#define I2C_SDA 21
-#define I2C_SCL 22
-#define ENC_A 25
-#define GPRS_PWRKEY 26
-#define HEATER 27
-#define SCREENBACKLIGHT 33
-#define ENC_B 32
-#define FAN_SPEED_FEEDBACK 35
-#define BABY_NTC_PIN 39
-#define AFE4490_ADC_READY NULL
-#define ON_OFF_SWITCH NULL
-#define AFE44XX_PWDN_PIN NULL
-
-// #define DISPLAY_CONTROLLER_IC ST7789V_CONTROLLER
-
-#elif (HW_NUM >= 9)
-// PINOUT
-#define TFT_DC 0
-#define AFE44XX_CS 2
-#define ENC_SWITCH 4
-#define BUZZER 5
-#define FAN 12
-#define PHOTOTHERAPY 13
-#define ACTUATORS_EN 14
-#define TFT_CS 15
-#define GSM_UART_TX_PIN 16
-#define GSM_UART_RX_PIN 17
-#define I2C_SDA 21
-#define I2C_SCL 22
-#define ENC_A 25
-#define GPRS_PWRKEY 26
-#define HEATER 27
-#define ENC_B 32
-#define SCREENBACKLIGHT 33
-#define ON_OFF_SWITCH 34
-#define FAN_SPEED_FEEDBACK 35
-#define AFE4490_ADC_READY 36
-#define BABY_NTC_PIN 39
-#define AFE44XX_PWDN_PIN NULL
-
-// #define DISPLAY_CONTROLLER_IC ST7789V_CONTROLLER
-#elif (HW_NUM == 8)
-#define TFT_DC 0
-#define ENC_SWITCH 4
-#define BUZZER 5
-#define FAN 12
-#define PHOTOTHERAPY 13
-#define HUMIDIFIER_CTL 14
-#define TFT_CS 15
-#define GSM_UART_TX_PIN 16
-#define GSM_UART_RX_PIN 17
-#define I2C_SDA 21
-#define I2C_SCL 22
-#define ENC_A 25
-#define GPRS_PWRKEY 26
-#define HEATER 27
-#define ENC_B 32
-#define SCREENBACKLIGHT 33
-#define ACTUATORS_EN 34 // fake pin
-#define SYSTEM_CURRENT_SENSOR 36
-#define BABY_NTC_PIN 39
-
-#define DISPLAY_CONTROLLER_IC ST7789V_CONTROLLER
-
-#else
-// Hardware
-// PINOUT
-
-#define AFE44XX_CS 2
-#define TFT_DC 4
-#define BUZZER 5
-#define TFT_CS 15 // fake GPIO
-#define GSM_UART_TX_PIN 16
-#define GSM_UART_RX_PIN 17
-#define I2C_SDA 21
-#define I2C_SCL 22
-#define ENC_A 25
-#define ENC_SWITCH 26
-#define HEATER 27
-#define ENC_B 32
-#define SCREENBACKLIGHT 33
-#define BABY_NTC_PIN 34
-#define AIR_NTC_PIN 35
-#define SYSTEM_CURRENT_SENSOR 36
-// #define SYSTEM_SHUNT 36
-
-#define GPIO_EXP_0 0 + GPIO_EXP_BASE
-#define GPIO_EXP_1 1 + GPIO_EXP_BASE
-#define GPIO_EXP_2 2 + GPIO_EXP_BASE
-#define GPIO_EXP_3 3 + GPIO_EXP_BASE
-#define GPIO_EXP_4 4 + GPIO_EXP_BASE
-#define GPIO_EXP_5 5 + GPIO_EXP_BASE
-#define GPIO_EXP_6 6 + GPIO_EXP_BASE
-#define GPIO_EXP_7 7 + GPIO_EXP_BASE
-#define GPIO_EXP_8 8 + GPIO_EXP_BASE
-#define GPIO_EXP_9 9 + GPIO_EXP_BASE
-#define GPIO_EXP_10 10 + GPIO_EXP_BASE
-#define GPIO_EXP_11 11 + GPIO_EXP_BASE
-#define GPIO_EXP_12 12 + GPIO_EXP_BASE
-#define GPIO_EXP_13 13 + GPIO_EXP_BASE
-#define GPIO_EXP_14 14 + GPIO_EXP_BASE
-#define GPIO_EXP_15 15 + GPIO_EXP_BASE
-
-#define UNUSED_GPIO_EXP0 GPIO_EXP_5
-#define UNUSED_GPIO_EXP1 GPIO_EXP_12
-#define UNUSED_GPIO_EXP2 GPIO_EXP_14
-#define UNUSED_GPIO_EXP3 GPIO_EXP_15
-
-#define ACTUATORS_EN GPIO_EXP_0
-#define GPRS_EN GPIO_EXP_1
-#define SD_CS GPIO_EXP_2
-#define FAN GPIO_EXP_3
-#define HUMIDIFIER_CTL GPIO_EXP_6
-#define PHOTOTHERAPY GPIO_EXP_7
-#define GPRS_PWRKEY GPIO_EXP_8
-#define TFT_CS_EXP GPIO_EXP_9
-#define TOUCH_IRQ GPIO_EXP_10
-#define TOUCH_CS GPIO_EXP_11
-#define TFT_RST GPIO_EXP_13
-
-#define DISPLAY_CONTROLLER_IC ILI9341_CONTROLLER
-
-#endif
 
 // Selección del puerto de depuración según modo USB
 #if ARDUINO_USB_MODE == 1
@@ -371,13 +171,6 @@
 #define BATTERY_SHUNT 27000   // miliohms
 #define USB_SHUNT 3           // miliohms
 #define HEATER_SHUNT 3        // miliohms
-#else
-#define SYSTEM_SHUNT 2        // miliohms
-#define BATTERY_SHUNT 27000   // miliohms
-#define FAN_SHUNT 100         // miliohms
-#define PHOTOTHERAPY_SHUNT 82 // miliohms
-#define HEATER_SHUNT 2        // miliohms
-#define USB_SHUNT 3           // miliohms
 #endif
 
 #define DISPLAY_DEFAULT_ROTATION 3
