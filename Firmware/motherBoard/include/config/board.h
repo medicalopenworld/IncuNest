00bcc7d5521f5aa71f20296cf6155dc791626013
pablo18393 <pablosanchez18393@hotmail.com>
2026-07-10 18:23:06 +0200

    chore(motherboard): bump FW version 17.3 -> 17.4

--- Firmware/motherBoard/include/config/board.h
+++ Firmware/motherBoard/include/config/board.h
@@ -27,7 +27,7 @@
 #endif
 #define HW_REVISION 'A'
 #define HWversion String(HW_NUM) + "." + String(HW_REVISION)
-#define FWversion "17.3"
+#define FWversion "17.4"
 #define WIFI_NAME "IncuNest"
 #define CURRENT_FIRMWARE_TITLE "IncuNest"
 // Set to true only on the HMI board
