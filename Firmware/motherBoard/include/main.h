a10b740e7ab69e1ca10a68cfe8d1ba220db3c2ab
pablo18393 <pablosanchez18393@hotmail.com>
2026-07-10 18:33:22 +0200

    chore(motherboard): lower AIR_TEMPERATURE_SET_MAX 38.5 -> 38

--- Firmware/motherBoard/include/main.h
+++ Firmware/motherBoard/include/main.h
@@ -296,7 +296,7 @@ typedef enum
 #define SKIN_TEMPERATURE_SET_MIN 35
 #define AIR_TEMPERATURE_SET_MIN 30
 #define SKIN_TEMPERATURE_SET_MAX 37.5
-#define AIR_TEMPERATURE_SET_MAX 38.5
+#define AIR_TEMPERATURE_SET_MAX 38
 
 // Encoder variables
 #define NUMENCODERS 1 // number of encoders in circuit
